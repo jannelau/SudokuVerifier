bb4543931125653a6f0b9ca153d1cc575b720a92
somename <someemail>
2016-09-15 15:11:25 +0300

    besouro automatic message

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -2,6 +2,8 @@
 public class SudokuVerifier {
 	
 	public int verify(String candidateSolution) {
+		String goodstring ="417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+		String badstring ="";
 		// returns 0 if the candidate solution is correct
 		return 0;
 		
